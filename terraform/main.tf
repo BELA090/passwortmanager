4dd0a250f3202a308355d4d44ce08708fe9cfa33
BELA090 <bela.luechinger@gmail.com>
2026-03-09 13:10:01 +0100

    add dynamodb table

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,16 @@ resource "aws_s3_bucket_website_configuration" "frontend_config" {
     suffix = "index.html"
   }
 }
+
+resource "aws_dynamodb_table" "password_table" {
+
+  name = "PasswortManagerDB"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+}
