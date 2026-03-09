5960597656f73cd96f8e1931037c2356709fd164
BELA090 <bela.luechinger@gmail.com>
2026-03-09 13:30:17 +0100

    change dynamodb table name

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_website_configuration" "frontend_config" {
 
 resource "aws_dynamodb_table" "password_table" {
 
-  name = "PasswortManagerDB"
+  name = "PasswortManagerDB2"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "id"
 
