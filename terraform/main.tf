c057c216c37afaeb863c86a455bf662341754bad
BELA090 <bela.luechinger@gmail.com>
2026-03-09 10:49:42 +0100

    fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,18 +13,3 @@ resource "aws_s3_bucket_website_configuration" "frontend_config" {
     suffix = "index.html"
   }
 }
-
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Sid = "PublicReadGetObject"
-      Effect = "Allow"
-      Principal = "*"
-      Action = "s3:GetObject"
-      Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
-    }]
-  })
-}
