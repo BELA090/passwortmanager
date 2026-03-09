44eee29721fc528a965be71bef5531f15cea8a64
BELA090 <bela.luechinger@gmail.com>
2026-03-09 13:47:20 +0100

    fix dynamodb table unique

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "frontend_bucket" {
-  bucket = "passwortmanager-bela090-website-2026"
+# zufällige ID für eindeutige Namen
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
+# S3 Bucket für Website
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "passwortmanager-bela090-${random_id.suffix.hex}"
+}
+
+# Static Website Hosting
 resource "aws_s3_bucket_website_configuration" "frontend_config" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
@@ -14,16 +21,16 @@ resource "aws_s3_bucket_website_configuration" "frontend_config" {
   }
 }
 
+# DynamoDB Tabelle
 resource "aws_dynamodb_table" "password_table" {
 
-  name = "PasswortManagerDB3"
+  name = "PasswortManager-${random_id.suffix.hex}"
 
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "id"
+  hash_key     = "id"
 
   attribute {
     name = "id"
     type = "S"
   }
-
 }
